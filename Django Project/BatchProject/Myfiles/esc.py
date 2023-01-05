a2215dd9c050102d5cab47d10b7d72d11ee485a8
SanketTops <sanket.tops@gmail.com>
2023-01-05 14:55:12 +0530

    Error MSG and UserAuth

---
+++ Django Project/BatchProject/Myfiles/esc.py
@@ -0,0 +1,6 @@
+#print("Hello 'Python!")
+#print('Hello "Python"!')
+
+print(R"Hello \nPython!")
+print(R"\tHello Python!")
+print(r"Hel\blo Python")
---
+++ Django Project/BatchProject/Myfiles/input.py
@@ -0,0 +1,10 @@
+#a=34
+
+#name='Sanket'
+"""name=input("Enter your name:")
+print("My name is",name)"""
+
+a=int(input("Enter value of A:"))#str
+b=int(input("Enter value of B:")) #str
+
+print("Sum:",a+b)
