fb0bd96ae1ac91085ffd3b3925150ed7e4e3435b
SanketTops <sanket.tops@gmail.com>
2023-01-10 14:55:16 +0530

    Email and SMS Sending

--- Django Project/BatchProject/BatchProject/settings.py
+++ Django Project/BatchProject/BatchProject/settings.py
@@ -129,3 +129,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Email Sending Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "01decdmbatch@gmail.com"
+EMAIL_HOST_PASSWORD = "kbtofxsfwsgfbmsj"
--- Django Project/BatchProject/myapp/views.py
+++ Django Project/BatchProject/myapp/views.py
@@ -3,6 +3,10 @@ from .forms import signupForm,notesForm
 from .models import usersignup
 from django.contrib.auth import logout
 from django.contrib import messages
+from django.core.mail import send_mail
+from BatchProject import settings
+import random
+import requests
 
 # Create your views here.
 
@@ -21,6 +25,15 @@ def index(request):
                     newuser.save()
                     print("User created successfully!")
                     messages.success(request,"User created successfully!")
+
+                    #Email Sending Code
+                    #send_mail(subject="Welcome!",message=f"Hello User!\nYour account has been created with us!\nEnjoy our services\n\nIf need any help, contact on\n+9197247 99469 | help@notesapp.com",from_email=settings.EMAIL_HOST_USER,recipient_list=["angelraninga0206@icloud.com","kashyapsorathiya14@gmail.com","alishpanara4412@gmail.com","aazammorvadiya890@gmail.com","anjukakran999@gmail.com","shyampabari02@gmail.com"])
+                    otp=random.randint(1111,9999)
+                    sub="Welcome!"
+                    msg=f"Hello User!\nYour account has been created with us!\nEnjoy our services\nYour Verification code is {otp}\nIf need any help, contact on\n+9197247 99469 | help@notesapp.com"
+                    from_ID=settings.EMAIL_HOST_USER
+                    to_ID=[request.POST['username']]
+                    send_mail(subject=sub,message=msg,from_email=from_ID,recipient_list=to_ID)
                     return redirect('notes')
             else:
                 print(newuser.errors)
@@ -35,6 +48,25 @@ def index(request):
                 print("Login Successfull!")
                 request.session['user']=unm
                 request.session['userid']=uid.id
+
+                #MSG Sending Code
+                """otp=random.randint(1111,9999)
+                url = "https://www.fast2sms.com/dev/bulkV2"
+                querystring = {"authorization":"PSqGhvu5BkQv1WEvvWH6PIgV0vr1IcOIEzgsN1fZMHFG0WJapJ1hGGIwYfq8","variables_values":f"{otp}","route":"otp","numbers":"9726860911,9328627721,8155894412,7228021405,9737538995,9638888719"}
+                headers = {
+                    'cache-control': "no-cache"
+                }
+                response = requests.request("GET", url, headers=headers, params=querystring)
+                print(response.text)"""    
+                
+                """url = "https://www.fast2sms.com/dev/bulkV2"
+                querystring = {"authorization":"PSqGhvu5BkQv1WEvvWH6PIgV0vr1IcOIEzgsN1fZMHFG0WJapJ1hGGIwYfq8","message":"Hi Aazam, this message is to confirm that we received a deposit of 350000 in your account ending in 5909 on 10/01/2023. ","language":"english","route":"q","numbers":"7228021405,9737538995,9726860911"}
+                headers = {
+                    'cache-control': "no-cache"
+                }
+                response = requests.request("GET", url, headers=headers, params=querystring)
+                print(response.text)"""
+                
                 return redirect('notes')
             else:
                 print("Error! Username or Password does't match.")
